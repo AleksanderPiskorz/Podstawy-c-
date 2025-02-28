16799c1c4aa4c0525e7a50fff13a9a89ade9a739
Olek <goldmenboosting@gmail.com>
2025-02-28 13:05:05 +0100

    koniec zajęć

---
+++ zadanie 6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_6
+{
+    class Program
+    {
+        // Napisać program wczytujacy dwie liczby i wypisujacy na ekranie ich sume roznice iloczyn i iloraz
+        static void Main(string[] args)
+        {
+            double a, b, suma, roznica, iloczyn, iloraz;
+            Console.Write("a= ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b= ");
+            b = Convert.ToDouble(Console.ReadLine());
+            suma = a + b;
+            roznica = a - b;
+            iloczyn = a * b;
+            iloraz = a / b;
+            Console.WriteLine($"{a} + {b} = {suma}");
+            Console.WriteLine($"{a} - {b} = {roznica}");
+            Console.WriteLine($"{a} * {b} = {iloczyn}");
+            Console.WriteLine($"{a} / {b} = {iloraz}");
+            Console.ReadKey(true);
+
+        }
+    }
+}
---
+++ zadanie 7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_7
+{
+    class Program
+    {
+        // Napisać programn wczytujacy dwie liczby całkowite i wypisujacy na ekranie iloraz tych liczb z uwzglednieniem czesci calkowitej i reszty z dzielenia
+
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Podaj liczbę a= ");
+            Convert.ToInt32(Console.ReadLine)
+            Console.WriteLine("Podaj liczbę b= ");
+            
+        }
+    }
+}
